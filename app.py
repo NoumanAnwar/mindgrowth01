f3c2ffc0d906e14055b3a1be646ef2010be508e0
NoumanAnwar <164159073+NoumanAnwar@users.noreply.github.com>
2025-03-10 16:22:33 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #streamlit
 import streamlit as st
 
-st.set_page_config(page_title="growth mindset project", project_icon="🌱")
+st.set_page_config(page_title="growth mindset project🌱")
 st.title("Growth Mindset Challenge: Web App with Streamlit")
 
 st.header("🌠Welcome to my Growth Journey")
